0f4c75400d597ad8ab787e73bf2fd7c3d1a2b6c8
Arif1951995 <40889693+Arif1951995@users.noreply.github.com>
2019-08-27 10:16:26 -0700

    refactor

--- script.js
+++ script.js
@@ -17,31 +17,30 @@ let constHours = 0;
 let constMins = 0;
 let constSeconds = 0;
 let totalTime = 0;
+let updateTime;
+let running = false;
 
 hoursInput.addEventListener('change', () => {
     hours = Number(hoursInput.value);
     hoursOutput.textContent = hours;
     constHours = hours;
-    // totalTime = constHours + constMins + constSeconds;
 
 
-    
+
 })
 minsInput.addEventListener('change', () => {
     mins = Number(minsInput.value);
     minsOutput.textContent = mins;
     constMins = mins;
-    // totalTime = constHours + constMins + constSeconds;
 
-    
+
 })
 secondsInput.addEventListener('change', () => {
     seconds = Number(secondsInput.value);
     secondsOutput.textContent = seconds;
     constSeconds = seconds;
-    // totalTime = constHours + constMins + constSeconds;
 
-    
+
 })
 
 
@@ -49,119 +48,72 @@ secondsInput.addEventListener('change', () => {
 
 startBtn.addEventListener('click', (e) => {
     stop = false;
-    
- 
 
- document.querySelectorAll('input').forEach(e => {
-     e.value = '';
- })
 
- 
- if(seconds > 0 || mins > 0 || hours > 0) {
-setInterval(() => {
-    
-        if(!stop) {
-            if(seconds <= 0) {
-                if(mins >= 0) {
-                    mins--;
-                    minsOutput.textContent = mins;
-                    seconds = 60;
-                }
-              
-         
+
+    document.querySelectorAll('input').forEach(e => {
+        e.value = '';
+    })
+
+
+    if (seconds > 0 || mins > 0 || hours > 0) {
+        if (!stop) {
+            if (!running) {
+                updateTime = setInterval(() => {
+                    if (!stop) {
+                        if (seconds <= 0) {
+                            if (mins >= 0) {
+                                mins--;
+                                minsOutput.textContent = mins;
+                                seconds = 60;
+                            }
+                        }
+                        if (seconds > 0) {
+                            seconds--;
+                            if (seconds <= 0 && mins <= 0 && hours <= 0) {
+                                stop = true;
+                            }
+                        }
+                        if (mins < 0) {
+                            if (hours > 0) {
+                                hours--;
+                                hoursOutput.textContent = hours;
+                                mins = 59;
+                            }
+                        }
+                        secondsOutput.textContent = seconds;
+                        minsOutput.textContent = mins;
+                        hoursOutput.textContent = hours;
+                        totalTime = ((constHours * 60) * 60) + (constMins * 60) + constSeconds;
+                        let remainingTime = `${(seconds + (mins * 60) + ((hours * 60)* 60))}`;
+                        let percentComplete = `${(remainingTime / totalTime) * 100}`;
+                        percentComplete = parseInt(percentComplete);
+                        countDownBar.style.width = `${100 - percentComplete}%`;
+                    }
+                }, 1000)
+                running = true;
             }
-
-            
-            if(seconds > 0 ) {
-                seconds--;
-                if(seconds <= 0 && mins <= 0 && hours <= 0) {
-                    stop = true;
-                    // secondsOutput = 0;
-                    // minsOutput = 0;
-                    // hoursOutput = 0;
-
-                }
-
-
-
-                console.log(seconds,mins,hours);
-
-        
-            }
-        
-           
-            if(mins < 0) { 
-                if(hours > 0) {
-                    hours--;
-                    hoursOutput.textContent = hours;
-                    mins = 59;
-                   
-                }
-                
-            } 
-
-            console.log(seconds);
-            
-        
-            secondsOutput.textContent = seconds;
-            minsOutput.textContent = mins;
-            hoursOutput.textContent = hours;
-            // console.log('a')
-            totalTime = ((constHours * 60) * 60) + (constMins * 60) + constSeconds;
-            
-            // console.log(totalTime);
-
-            // (300 / 2000) * 100 
-            
-
-            let remainingTime = `${(seconds + (mins * 60) + ((hours * 60)* 60))}`;
-           let percentComplete = `${(remainingTime / totalTime) * 100}`;
-           percentComplete = parseInt(percentComplete); 
-        //    console.log(percentComplete);
-        countDownBar.style.width = `${100 - percentComplete}%`;
-        // countDownBar.textContent = `${100 - percentComplete}% Completed`;
-
-
-
-            
-
-            //  console.log((seconds + (mins * 60) + (hours * 60 * 60)) * percentComplete)
-
- 
-    
         }
-    
-    
-   
-},1000)
- }
-
-
-       
-
-
-    
-
-   
-
-
+    }
 })
 
 
 
 stopBtn.addEventListener('click', () => {
+    clearInterval(updateTime);
     stop = true;
+    running = false;
+
 })
 resetBtn.addEventListener('click', () => {
-     hours = 0;
- mins = 0;
- seconds = 0;
- stop = true;
- hoursOutput.textContent = 0;
- minsOutput.textContent = 0;
- secondsOutput.textContent = 0;
-
+    hours = 0;
+    mins = 0;
+    seconds = 0;
+    stop = true;
+    hoursOutput.textContent = 0;
+    minsOutput.textContent = 0;
+    secondsOutput.textContent = 0;
+    clearInterval(updateTime);
+    countDownBar.style.width = `${0}%`;
+    running = false;
 })
-
-// let a = (1000 - 100);
-// console.log(`${a}`)
